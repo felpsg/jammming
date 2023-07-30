3767cf17a2a9c4f091a665257921eeff239ffb31
felps <felipetec.sso@gmail.com>
2023-07-30 14:54:52 -0300

    Etapas finais do app Jammming

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-// App.jsx
-import React from 'react';
-import './App.css';
-import SearchBar from './components/SearchBar/SearchBar';
+import React, { useState } from 'react';
+import '../App.css';
+import SearchBar from './SearchBar';
+import Playlist from './Playlist';
+import Spotify from './Spotify';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 class="jammingTitle">
-          Ja<span className="spanTitle">mmm</span>ing
-        </h1>
-        <SearchBar />
-      </div>
+const App = () => {
+  const [playlistName, setPlaylistName] = useState('Nova Playlist');
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+
+  const addTrack = (track) => {
+    if (!playlistTracks.some((t) => t.id === track.id)) {
+      setPlaylistTracks((prevTracks) => [...prevTracks, track]);
+    }
+  };
+
+  const removeTrack = (track) => {
+    setPlaylistTracks((prevTracks) =>
+      prevTracks.filter((t) => t.id !== track.id),
     );
-  }
-}
+  };
+
+  const updatePlaylistName = (name) => {
+    setPlaylistName(name);
+  };
+
+  const savePlaylist = async () => {
+    const trackUris = playlistTracks.map((track) => track.uri);
+    await Spotify.savePlaylist(playlistName, trackUris);
+    setPlaylistName('Nova Playlist');
+    setPlaylistTracks([]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Jammming</h1>
+      <SearchBar onAdd={addTrack} />
+      <Playlist
+        name={playlistName}
+        tracks={playlistTracks}
+        onRemove={removeTrack}
+        onNameChange={updatePlaylistName}
+        onSave={savePlaylist}
+      />
+    </div>
+  );
+};
 
 export default App;
