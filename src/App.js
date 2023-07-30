087c251c712068891f64a6c45126607a82cca288
felps <felipetec.sso@gmail.com>
2023-07-29 22:36:26 -0300

    add os componentes da aplicacao

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 
-
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1 class="jammingTitle">Ja<span className='spanTitle'>mmm</span>ing</h1>
+        <h1 class="jammingTitle">
+          Ja<span className="spanTitle">mmm</span>ing
+        </h1>
         <SearchBar />
-        {/* Outros componentes vão aqui */}
       </div>
     );
   }
